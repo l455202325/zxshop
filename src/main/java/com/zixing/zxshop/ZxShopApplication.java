f9cdc78f3bf04b68dfd1594377560edb588d4428
zixing <liubaoping325@163.com>
2018-10-02 11:28:12 +0800

    第一次提交

---
+++ src/main/java/com/zixing/zxshop/ZxShopApplication.java
@@ -0,0 +1,12 @@
+package com.zixing.zxshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZxShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZxShopApplication.class, args);
+	}
+}
